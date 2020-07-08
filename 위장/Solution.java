0587c2ca5ca58b9c3ec88e1537f9cfec4d846008
taylous <taylous99@gmail.com>
2020-07-08 22:40:18 +0900

    해시 위장

---
+++ 위장/Solution.java
@@ -0,0 +1,33 @@
+package 위장;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        System.out.println(new Solution().solution(new String[][]{
+                {"yellow_hat", "headgear"},
+                {"blue_sunglasses", "eyewear"},
+                {"green_turban", "headgear"}
+        }));
+        System.out.println(new Solution().solution(new String[][]{
+                {"crow_mask", "face"},
+                {"blue_sunglasses", "face"},
+                {"smoky_makeup", "face"}
+        }));
+    }
+
+    public int solution(String[][] clothes) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        int answer = 1;
+
+        for(String[] cloth : clothes)
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
+        for(int value : map.values())
+            answer *= (value + 1);
+        return answer - 1;
+    }
+}
